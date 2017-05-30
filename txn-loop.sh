33bfb926d455c3e99b4ad1aefa31d13118b20810
ianco <iancostanzo@gmail.com>
2017-05-30 05:48:20 +0100

    transaction scripts

---
+++ txn-loop.sh
@@ -0,0 +1,6 @@
+for i in {1..100..1}
+do
+./txn.sh | telnet
+sleep 2
+done
+
---
+++ txn.sh
@@ -0,0 +1,18 @@
+echo open localhost 2000
+echo wallet
+sleep 2
+echo send 0 Qe1563a15fe6ffae964473d11180aaace207bcb1ed1ac570dfb46684421f7bb4f10eb .01
+sleep 1
+echo send 0 Qe1563a15fe6ffae964473d11180aaace207bcb1ed1ac570dfb46684421f7bb4f10eb .002
+sleep 1
+echo send 0 Qe1563a15fe6ffae964473d11180aaace207bcb1ed1ac570dfb46684421f7bb4f10eb .0003
+sleep 1
+echo send 0 Qe1563a15fe6ffae964473d11180aaace207bcb1ed1ac570dfb46684421f7bb4f10eb .004
+sleep 1
+echo send 0 Qe1563a15fe6ffae964473d11180aaace207bcb1ed1ac570dfb46684421f7bb4f10eb .05
+sleep 1
+echo wallet
+sleep 1
+sleep 2
+echo exit
+
